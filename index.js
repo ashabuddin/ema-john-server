049e16d28139650938a10a47978e981d4c22b395
Ashab <ashabuddin54321@gmail.com>
2021-08-23 08:51:22 -0700

    chenge

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
  const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 
+require('dotenv').config();
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kxbrw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
